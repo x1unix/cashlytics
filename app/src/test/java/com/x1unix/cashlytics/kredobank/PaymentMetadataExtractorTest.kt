360f6c10838c83406f67d2aee32df389807c8e0f
x1unix <denis0051@gmail.com>
2018-07-02 04:08:25 +0300

    tests: add withdrawal and refill

--- app/src/test/java/com/x1unix/cashlytics/kredobank/PaymentMetadataExtractorTest.kt
+++ app/src/test/java/com/x1unix/cashlytics/kredobank/PaymentMetadataExtractorTest.kt
@@ -7,6 +7,9 @@ import com.x1unix.cashlytics.providers.kredobank.extractors.PaymentDataExtractor
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.exp
+
+const val BANK_NAME = "KREDOBANK"
 
 class PaymentMetadataExtractorTest {
     private val extractor = PaymentDataExtractor()
@@ -23,6 +26,30 @@ class PaymentMetadataExtractorTest {
     fun parseOfflinePurchase() {
         val expected = PaymentMetadata(PaymentType.Purchase, "MAGAZINPRODUKTIVARSEN")
         val result = extractor.extractData(Messages.Purchase)
+
+        assertEquals(result.result, expected)
+    }
+
+    @Test
+    fun parseTransfer() {
+        val expected = PaymentMetadata(PaymentType.Transfer, BANK_NAME)
+        val result = extractor.extractData(Messages.Transfer)
+
+        assertEquals(result.result, expected)
+    }
+
+    @Test
+    fun parseWithdrawal() {
+        val expected = PaymentMetadata(PaymentType.Withdrawal, BANK_NAME)
+        val result = extractor.extractData(Messages.Withdrawal)
+
+        assertEquals(result.result, expected)
+    }
+
+    @Test
+    fun parseRefill() {
+        val expected = PaymentMetadata(PaymentType.Refill, BANK_NAME)
+        val result = extractor.extractData(Messages.Refill)
 
         assertEquals(result.result, expected)
     }
