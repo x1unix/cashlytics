6ed6cced0cfc4ec8d8fba1c497eb0f67e7d1543a
x1unix <denis0051@gmail.com>
2018-07-07 04:22:17 +0300

    tests: kredobank - add rest balance data fixtures

--- app/src/test/java/com/x1unix/cashlytics/kredobank/fixtures/Balance.kt
+++ app/src/test/java/com/x1unix/cashlytics/kredobank/fixtures/Balance.kt
@@ -11,7 +11,7 @@ object Balance {
     const val Purchase = ", UA 1267.18UAH CARD**2111 ZALISHOK 24507.17 UAH OVER 50000.00  DOSTUPNO 74507.17UAH"
     const val InternetPurchase = ", GB 59.00UAH CARD**2111 ZALISHOK 22805.84 UAH OVER 50000.00  DOSTUPNO 72805.84UAH"
     const val Refill = "65789.14UAH CARD**2111 ZALISHOK 27723.75 UAH OVER 50000.00 UAH DOSTUPNO 77723.75UAH"
-    const val Debit = "193.08UAH CARD**2111 ZALISHOK 27530.67 UAH OVERDRAFT 50000.00 UAH DOSTUPNO 77530.67UAH"
+    const val Debit = "193.08UAH CARD**2111 ZALISHOK 27530.67 UAH OVERDRAFT 45000.00 UAH DOSTUPNO 77530.67UAH"
 
     val TransferChange: BalanceChange
         get() = BalanceChange(uah(1000.0), uah(26174.35), uah(50000.0), uah(76174.35))
@@ -21,6 +21,15 @@ object Balance {
 
     val PurchaseChange: BalanceChange
         get() = BalanceChange(uah(1267.18), uah(24507.17), uah(50000.00), uah(74507.17))
+
+    val InternetPurchaseChange: BalanceChange
+        get() = BalanceChange(uah(59.0), uah(22805.84), uah(50000.00), uah(72805.84))
+
+    val RefillChange: BalanceChange
+        get() = BalanceChange(uah(65789.14), uah(27723.75), uah(50000.00), uah(77723.75))
+
+    val DebitChange: BalanceChange
+        get() = BalanceChange(uah(193.08), uah(27530.67), uah(45000.00), uah(77530.67))
 
 
 
